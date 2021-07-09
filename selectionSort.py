ddcf457d6b2c7c595672a0409d41f6afc4f5a2bd
Jaymin <jayminwest@gmail.com>
2021-07-08 18:14:19 -0600

    Added better colors to Selection Sort

--- selectionSort.py
+++ selectionSort.py
@@ -1,4 +1,3 @@
-#ALL OF THIS TAKEN FROM THIS TUTORIAL SERIES: Python GUI with Tkinter Tutorial | Beginner Friendly | Sorting Algorithm Visualization https://www.youtube.com/watch?v=y6Nz56RHK_Q&ab_channel=Dennis
 import time
 
 def selection_sort(data, drawData, timeTick):
@@ -6,9 +5,10 @@ def selection_sort(data, drawData, timeTick):
 
     for i in indexingLength:
         minValue = i #This changes the default min with each itteration
+        drawData(data, ['green' if x < i else 'red' for x in range(len(data))])
 
         for j in range(i + 1, len(data)): # All items to the right of where i is
-            drawData(data, ['green' if x == j or x == i else 'red' for x in range(len(data))])
+            drawData(data, ['grey' if x == j else 'green' if x == i else 'red' for x in range(len(data))])
             time.sleep(timeTick)
 
             if data[j] < data[minValue]:
